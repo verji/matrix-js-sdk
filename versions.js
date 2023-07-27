caba419c2de8e8b2c2c6af5ac600cc233a7f82c2
RiotRobot <releases@riot.im>
2023-07-27 08:31:51 +0000

    Update docs

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 "use strict"
 export const DOC_VERSIONS = [
 	'stable',
+	'v27.1',
 	'v27.0',
 	'v26.2',
 	'v26.1',
@@ -31,4 +32,5 @@ export const DOC_VERSIONS = [
 	'v19.0',
 	'v18.1',
 	'v18.0',
+	'dev',
 ];
