b7f4cdfbb1111d6b38b13f00817d75e3b4c052a0
John Tore Simonsen <john.tore.simonsen@rosberg.com>
2025-11-18 11:13:59 +0100

    lint

--- src/client.ts
+++ src/client.ts
@@ -8548,10 +8548,10 @@ export class MatrixClient extends TypedEventEmitter<EmittedEvents, ClientEventHa
      * @param options.limit - the maximum number of results to return. The server will apply a limit if unspecified.
      * @returns Promise which resolves: an array of results.
      */
-    public searchUserDirectory({ term, limit }: { term: string; limit?: number }, extraBodyArgs: {[key:string]: string}|null = {}, extraOptions: IRequestOpts = {}): Promise<IUserDirectoryResponse> {
+    public searchUserDirectory({ term, limit }: { term: string; limit?: number }, extraBodyArgs: {[key: string]: string}|null = {}, extraOptions: IRequestOpts = {}): Promise<IUserDirectoryResponse> {
         const body: Body = {
             search_term: term, 
-            ...extraBodyArgs
+            ...extraBodyArgs,
         };
 
         if (limit !== undefined) {
